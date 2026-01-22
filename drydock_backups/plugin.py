aba6187777c632d9c97c19531d84bbf7823ab1dd
beto Fandino <beto3971@gmail.com>
2026-01-21 22:04:40 -0400

    fix: return list of plugins

--- drydock_backups/plugin.py
+++ drydock_backups/plugin.py
@@ -108,10 +108,11 @@ with open(
     tutor_hooks.Filters.CLI_DO_INIT_TASKS.add_item(("mysql", fi.read()), priority=tutor_hooks.priorities.HIGH)
 
 @tutor_hooks.Filters.PLUGINS_LOADED.add()
-def _add_minio_init(_list_name:list[str]) -> None:
-    if "minio" in _list_name:
+def _add_minio_init(_plugins_list:list[str]) -> list[str]:
+    if "minio" in _plugins_list:
         with open(
             str(importlib_resources.files("drydock_backups") / "templates" / "drydock_backups" / "task" / "minio" / "init"),
             encoding="utf-8",
         ) as fi:
             tutor_hooks.Filters.CLI_DO_INIT_TASKS.add_item(("minio", fi.read()), priority=tutor_hooks.priorities.HIGH)
+    return _plugins_list
